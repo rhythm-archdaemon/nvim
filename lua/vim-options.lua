c73c5669e295dcb59e54fcf221a489a7befcb071
rhythm-archdaemon <rhythmbhattarai@proton.me>
2026-06-14 20:24:35 +0545

    minor tweaks

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -8,7 +8,14 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 
+-- options: set leader button -> [space]
 vim.g.mapleader = " "
 
+-- options: indent in visual mode
 vim.keymap.set("v", ">", ">gv")
 vim.keymap.set("v", "<", "<gv")
+
+-- options: shows line number
+vim.opt.number = true
+vim.opt.relativenumber = false
+vim.opt.signcolumn = "yes"
